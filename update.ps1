b8a895cb7a32b7389d8d287e427c27dd40e4d779
elkeinkrad <elkein_krad@nate.com>
2020-12-10 19:02:59 +0900

    update powershell script

---
+++ update.ps1
@@ -0,0 +1,7 @@
+Write-Output "generate rr_raw.html..."
+python generate.py raw/rr_raw.html rr_raw.html --raw
+
+Write-Output "generate rr.html..."
+python generate.py raw/rr.html rr.html
+
+Write-Output "terminated..."
