83ccbeb63b9ed3e2fabbf919425714c3c981f17c
elkeinkrad <elkein_krad@nate.com>
2020-12-14 11:41:42 +0900

    update bash

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 echo -e "generate rule_reference.html..."
 python3 generate.py raw/rule_reference.html rule_reference.html
 
-echo -e "generate rr_ongoing.html..."
-python3 generate.py raw/rr_ongoing.html rr_ongoing.html
+#echo -e "generate rr_ongoing.html..."
+#python3 generate.py raw/rr_ongoing.html rr_ongoing.html
 
 echo -e "terminated..."
