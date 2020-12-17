a5f9d6bee7417662dab2948d89cd5a408519b5d9
elkeinkrad <elkein_krad@nate.com>
2020-12-17 15:04:43 +0900

    Update taboo

--- update.ps1
+++ update.ps1
@@ -1,10 +1,16 @@
 Write-Output "generate rule_reference.html..."
 python generate.py raw/rule_reference.html rule_reference.html
 
+Write-Output "generate notes.html..."
+python generate.py raw/notes.html notes.html --rr rule_reference.html
+
 Write-Output "generate faq.html..."
-python generate.py raw/faq.html faq.html --rr rule_reference.html
+python generate.py raw/faq.html faq.html --rr rule_reference.html --faq notes.html
 
 Write-Output "generate errata.html..."
 python generate.py raw/errata.html errata.html --nolink
+
+Write-Output "generate taboo.html..."
+python generate.py raw/taboo.html taboo.html --nolink
 
 Write-Output "terminated..."
--- update.sh
+++ update.sh
@@ -11,4 +11,7 @@ python3 generate.py raw/faq.html faq.html --rr rule_reference.html --faq notes.h
 echo -e "generate errata.html..."
 python3 generate.py raw/errata.html errata.html --nolink
 
+echo -e "generate taboo.html..."
+python3 generate.py raw/taboo.html taboo.html --nolink
+
 echo -e "terminated..."
