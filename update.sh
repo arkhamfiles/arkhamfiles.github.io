243e6af96549b113d40606fd4200fefd257985cb
elkeinkrad <elkein_krad@nate.com>
2020-12-17 13:54:26 +0900

    Seperate FAQ and Notes

--- update.sh
+++ update.sh
@@ -2,8 +2,11 @@
 echo -e "generate rule_reference.html..."
 python3 generate.py raw/rule_reference.html rule_reference.html
 
+echo -e "generate notes.html..."
+python3 generate.py raw/notes.html notes.html --rr rule_reference.html
+
 echo -e "generate faq.html..."
-python3 generate.py raw/faq.html faq.html --rr rule_reference.html
+python3 generate.py raw/faq.html faq.html --rr rule_reference.html --faq notes.html
 
 echo -e "generate errata.html..."
 python3 generate.py raw/errata.html errata.html --nolink
