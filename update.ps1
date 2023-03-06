e99c1933cef49f122484de76bb6ca8f2277069a0
elkeinkrad <elkein_krad@nate.com>
2023-03-06 15:31:53 +0900

    하우스룰 브랜치 구조 수정

--- update.ps1
+++ update.ps1
@@ -1,35 +1,4 @@
-Write-Output "update card images..."
-python download_cards.py
-
-Write-Output "generate rule_reference.html..."
-python generate.py raw/rule_reference.html rule_reference.html
-
-Write-Output "generate notes.html..."
-python generate.py raw/notes.html notes.html --rr rule_reference.html
-
-Write-Output "generate faq.html..."
-python generate.py raw/faq.html faq.html --rr rule_reference.html --faq notes.html
-python generate.py raw/faq_legacy.html faq_legacy.html --rr rule_reference.html --faq notes.html
-
-Write-Output "generate errata.html..."
-python generate.py raw/errata.html errata.html --nolink
-
-Write-Output "generate taboo.html..."
-python generate.py raw/taboo.html taboo.html --nolink
-
-Write-Output "generate ultimatums.html..."
-python generate.py raw/ultimatums.html ultimatums.html --nolink
-
-Write-Output "generate starter_deck.html..."
-python generate.py raw/starter_deck.html starter_deck.html --nolink
-
-Write-Output "generate index.html..."
-python generate.py raw/index.html index.html --nolink
-
-Write-Output "generate test.html..."
-python generate.py raw/test.html test.html --nolink
-
-Write-Output "generate utility.html..."
-python generate.py raw/utility.html utility.html --nolink
+Write-Output "generate taboolight.html..."
+python generate.py raw/taboolight.html taboolight.html --nolink
 
 Write-Output "terminated..."
