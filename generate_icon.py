af6bae41fbd978c3e740b4cdc3e4ef2d94c1ec58
elkeinkrad <elkein_krad@nate.com>
2023-04-18 19:05:57 +0900

    폰트 자동화 코드 완성

--- generate_icon.py
+++ generate_icon.py
@@ -1,127 +1,229 @@
 #!/usr/bin/env python3
-import os
+"""generate font file for icon
+
+This script will generate icon font file using svg files.
+
+Requirements:
+ * info.json for metadata of icon data
+ * default.ttx for metadata of font information
+
+LICENCE:
+Please check the README.md of this repo.
+This script will be distributed via MIT LICENCE.
+However, SVG file and FONT has copyright.
+
+If you need ttx file, you can find free font file and extract via
+font = TTFont("path_of_font")
+font.saveXML("path_to_save.ttx")
+Then, manually modify it
+(delete glyf table, modify name table...)
+
+"""
+
+from os import PathLike
+import argparse
 import json
-import logging
-import time
-from typing import Dict, List
+import re
 from pathlib import Path
-from fontTools import ttLib
+from collections import Counter
+from fontTools.ttLib.ttFont import TTFont
+from fontTools.pens.ttGlyphPen import TTGlyphPen
+from fontTools.pens.boundsPen import BoundsPen
+from fontTools.misc import transform
+from fontTools import svgLib
+from ftCLI.Lib.Font import Font
+from ftCLI.Lib.converters.ttf_to_otf import TrueTypeToCFF
+from ftCLI.Lib.converters.otf_to_ttf import CFFToTrueType
+from ftCLI.Lib.converters.sfnt_to_web import SFNTToWeb
 
-def check_update_necessary(base: Path, want: Path) -> bool:
-    """check whether update is necessary based on fixed time
 
-    Args:
-        base (Path): target based file
-        want (Path): generated
-
-    Returns:
-        bool: True if update is necessary
-    """
-    logger = logging.getLogger("check_update")
-    if not want.exists():
-        logger.debug("target doesn't exist: %s", want)
-        return True
-    baseinfo = base.stat()
-    wantinfo = want.stat()
-    logger.debug("basetime: %d, wanttime: %d", baseinfo.st_mtime_ns, wantinfo.st_mtime_ns)
-    if baseinfo.st_mtime_ns > wantinfo.st_mtime_ns:
-        logger.debug("base is newer; updated")
-        return True
-    logger.debug("base is older; skip")
-    return False
-
-# def generate_glyph(font: fontforge.font, key: str, name: str, path: str) -> fontforge.glyph:
-#     glyph = font.createChar(ord(key), name)
-#     glyph.importOutlines(path)
-#     return glyph
-
-# def generate_fonts():
-#     with open('svgs/info.json') as filept:
-#         data = json.load(filept)
-#     font = fontforge.font()
-#     for item in data:
-#         glyph = font.createChar(ord(item['char']), item['name'])
-#         glyph.importOutlines(os.path.join('svgs', item['path']))
-#     font.generate("fonts/arkham-symbols.ttf")
-#     font.generate("fonts/arkham-symbols.otf")
-#     font.generate("fonts/arkham-symbols.woff")
-#     font.generate("fonts/arkham-symbols.svg")
-
-def generate_fonts(path_save: Path):
-    extension = path_save.suffix
-    sfntVersion = '\x00\x01\x00\x00'
-    flavor = None
-    if extension == '.ttf':
-        pass
-    elif extension == '.otf':
-        sfntVersion = 'OTTO'
-    elif extension == '.woff':
-        flavor = 'woff'
-    elif extension == '.woff2':
-        flavor = 'woff2'
-    elif extension == '.svg':
-        pass
-    else:
-        raise NotImplementedError(f"unknown extension: {extension}")
-    
-    tt = ttLib.TTFont(sfntVersion=sfntVersion, flavor=flavor)
-    
-
-def generate_css(path_info: Path, path_font: Path, path_css: Path):
-    """generate css file from info
+def generate_ttf(path_info: PathLike, path_xml: PathLike, path_save: PathLike, refine: bool=False):
+    """generate truetype font from files
 
     Args:
-        path_info (Path): information file for symbols
-        path_font (Path): save path for font (without extension)
-        path_css (Path): save path for css
+        path_info (Path): path of information file
+        path_xml (Path): path of xml file for fonttools
+        path_save (Path): font save path
+        refine (bool, optional): whether refinement performed using ftCLI. Defaults to False.
     """
+    path_info = Path(path_info)
+    path_xml = Path(path_xml)
+    path_save = Path(path_save)
+
+    if not path_info.exists():
+        raise FileNotFoundError(path_info)
+    if not path_xml.exists():
+        raise FileNotFoundError(path_xml)
+    if not path_save.parent.is_dir():
+        raise FileNotFoundError("parent of save path is directory:", path_save)
+
+    with path_info.open(encoding='utf-8') as file:
+        data = json.loads(file.read())
+
+    cnt = Counter()
+    for info in data:
+        cnt[info['char']] += 1
+    if cnt.most_common()[0][1] > 1:
+        illegals = [x for x, y in cnt.items() if y > 1]
+        raise ValueError("Duplicated Char Mapping in info file:", illegals)
+    del cnt
+
+    font = TTFont(sfntVersion="\x00\x01\x00\x00", flavor=False)
+    font.importXML(path_xml)
+
+    sample_text = ''
+    for info in data:
+        name = info['name']
+        path = path_info.parent / info['path']
+        svg = svgLib.path.SVGPath(path)
+        el = svg.root.find("{http://www.w3.org/2000/svg}g")
+        if el is not None and el.get('transform', None) is not None:
+            tr = el.get('transform', None)
+            number = r'([+-]?[0-9]+\.[0-9]+)'
+            matchtxt = re.compile(
+                r'translate\('+number+r','+number+r'\) scale\('+number+r','+number+r'\)'
+            )
+            match = matchtxt.match(tr)
+            match = tuple(map(float, match.groups()))
+            svg.transform = transform.Identity.translate(
+                match[0], match[1]
+            ).scale(match[2], match[3])
+        bpen = BoundsPen(None)
+        svg.draw(bpen)
+        xMin, yMin, xMax, yMax = bpen.bounds
+        scale = min(950/(xMax-xMin), 800/(yMax-yMin))
+        dx = 500 - 0.5*scale*(xMin+xMax)
+        dy = 500 + 0.5*scale*(yMin+yMax)
+        tf = transform.Identity.translate(dx, dy).scale(scale, -scale)
+        pen = TTGlyphPen({ord(info['char']): name})
+        if svg.transform is None:
+            svg.transform = tf
+        else:
+            svg.transform = tf.transform(svg.transform)
+        svg.transform = transform.Identity.translate(0, -200).transform(svg.transform)
+        svg.draw(pen)
+        glyph = pen.glyph()
+        glyph.trim()
+        font['glyf'][name] = glyph
+        for cmap in font['cmap'].tables:
+            cmap.cmap[ord(info['char'])] = name
+        sample_text += info['char']
+    font['name'].addName(sample_text, ((0, 3, 0x0), (1, 0, 0x0), (3, 1, 0x0409)), 18)
+
+    for name, glyph in font['glyf'].glyphs.items():
+        glyph.recalcBounds(font)
+        glyph.xMin = max(0, glyph.xMin)
+        glyph.yMin = max(0, glyph.yMin)
+        glyph.xMax = min(950, glyph.xMax)
+        glyph.yMax = min(950, glyph.yMax)
+        if name in font['hmtx'].metrics:
+            continue
+        font['hmtx'][name] = 950, glyph.xMin
+
+    font.setGlyphOrder(font['glyf'].glyphOrder)
+
+    for table in font.tables.values():
+        if hasattr(table, 'compile'):
+            table.compile(font)
+
+    font.save(path_save)
+    if refine:
+        font = Font(path_save)
+        t2o = TrueTypeToCFF(font)
+        t2o.run()
+        o2t = CFFToTrueType(t2o.font)
+        o2t.run()
+        o2t.font.save(path_save)
+
+def convert_others(path_ttf: PathLike, refine: bool=True):
+    """generate other fonts from truetype fonts
+    - otf, woff, woff2
+    at same folder with different suffix.
+
+    Args:
+        path_ttf (PathLike): path of truetype font
+        refine (bool, optional): whether refinement performed for TTF using ftCLI. Defaults to True.
+    """
+    path_ttf = Path(path_ttf)
+    if path_ttf.suffix != '.ttf':
+        raise ValueError("suffix of TTF file should be .ttf")
+    if not path_ttf.exists():
+        raise FileNotFoundError(path_ttf)
+    font = Font(path_ttf)
+    ttf2otf = TrueTypeToCFF(font)
+    ttf2otf.run()
+    ttf2otf.font.save(path_ttf.with_suffix('.otf'))
+    if refine:
+        otf2ttf = CFFToTrueType(Font(path_ttf.with_suffix('.otf')))
+        otf2ttf.run()
+        otf2ttf.font.save(path_ttf.with_suffix('.ttf'))
+    sfnt2web = SFNTToWeb(Font(path_ttf.with_suffix('.ttf')), 'woff')
+    sfnt2web.run()
+    sfnt2web.font.save(path_ttf.with_suffix('.woff'))
+    sfnt2web = SFNTToWeb(Font(path_ttf.with_suffix('.ttf')), 'woff2')
+    sfnt2web.run()
+    sfnt2web.font.save(path_ttf.with_suffix('.woff2'))
+
+def generate_css(path_info: PathLike, path_font: PathLike, path_css: PathLike):
+    """generate css file for icon font
+
+    Args:
+        path_info (PathLike): path of font information json
+        path_font (PathLike): path of font (anyone)
+        path_css (PathLike): paht of css generation
+    """
+    path_info = Path(path_info)
+    path_font = Path(path_font)
+    path_css = Path(path_css)
+    if not path_info.exists():
+        raise FileNotFoundError(path_info)
+    
     with path_info.open(encoding='utf-8') as filept:
-        data: List[Dict[str, str]] = json.load(filept)
-    relative_font = os.path.relpath(path_font, path_css.parent).replace('\\', '/')
+        data = json.load(filept)
+    font = TTFont(path_font)
+    files = []
+    for fp in path_font.parent.iterdir():
+        if fp.stem == path_font.stem:
+            string = str(fp).replace('\\', '/')
+            if fp.suffix == '.otf':
+                files.append((string, 'opentype'))
+            elif fp.suffix == '.ttf':
+                files.append((string, 'truetype'))
+            else:
+                files.append((string, fp.suffix[1:]))
+    s = ",\n    ".join(map(lambda x: f"url('../{x[0]}') format('{x[1]}')", files))
+    header = f"""@font-face {{
+    font-family: '{font['name'].getBestFamilyName()}';
+    src: {s};
+    font-weight: normal;
+    font-style: normal;
+}}
+[class^="symbol-"],[class*=" symbol-"] {{
+    font-family: '{font['name'].getBestFamilyName()}';
+    font-style: normal;
+    font-weight: normal;
+    font-variant: normal;
+    text-transform: none;
+    line-height: 1;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale
+}}"""
     with path_css.open('w', encoding='utf-8') as filept:
-        print("@font-face {",
-              "    font-family: 'arkhamsymbols';",
-              "    src:",
-              f"        url('{relative_font + '.otf'}') format('opentype'), ",
-              f"        url('{relative_font + '.ttf'}') format('truetype'), ",
-              f"        url('{relative_font + '.woff'}') format('woff'), ",
-              f"        url('{relative_font + '.svg'}') format('svg');",
-              "    font-weight: normal;",
-              "    font-style: normal;\n}\n",
-             '[class^="symbol-"],[class*=" symbol-"] {',
-             "    font-family: 'arkhamsymbols';",
-             "    font-style: normal;",
-             "    font-weight: normal;",
-             "    font-variant: normal;",
-             "    text-transform: none;",
-             "    line-height: 1;",
-             "    -webkit-font-smoothing: antialiased;",
-             "    -moz-osx-font-smoothing: grayscale\n}\n",
-             sep="\n", file=filept
-        )
+        print(header, file=filept)
+        print(file=filept)
         for item in data:
             filept.write(f'.symbol-{item["name"]}:before {{\n    content: "{item["char"]}"\n}}\n\n')
 
-def main(path_info: os.PathLike, path_font: os.PathLike, path_css: os.PathLike):
+def main():
     """main function
-
-    Args:
-        path_info (os.PathLike): information file for symbols
-        path_font (os.PathLike): save path for font (without extension)
-        path_css (os.PathLike): save path for css
+    TODO: argument parser
     """
-    path_info = Path(path_info)
-    path_font = Path(path_font).with_suffix('')
-    path_css = Path(path_css)
-    # is_update = check_update_necessary(path_info, path_font.with_suffix('.ttf'))
-    # if not is_update:
-    #     print('the font is not generated (no update exists).')
-    #     return
-    start_time = time.time()
-    # generate_fonts()
-    generate_css(path_info, path_font, path_css)
-    print(f'generate done: {(time.time()-start_time)*1000:.2f}ms')
+    parser = argparse.ArgumentParser(
+        description="Autometric font generator for icon files. parser will be updated"
+    )
+    generate_ttf("svgs/info.json", "svgs/default.ttx", "fonts/arkham-symbols.ttf", True)
+    convert_others("fonts/arkham-symbols.ttf", True)
+    generate_css("svgs/info.json", "fonts/arkham-symbols.ttf", "css/symbols.css")
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    main("svgs/info.json", "fonts/arkham-symbols", "css/symbols.css")
+    main()
