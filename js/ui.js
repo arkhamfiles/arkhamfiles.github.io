bde0e4749391e980b43bca0c5583eb7d0f905503
Chungkeun Lee <41584047+elkeinkrad@users.noreply.github.com>
2023-08-31 09:31:36 +0900

    1.5 init

--- js/ui.js
+++ js/ui.js
@@ -9,39 +9,39 @@ window.onload = function () {
   }
 
   window.toggleViewAll.onclick = function() {
-    toggleViewSelector('.eoep, eoec, .tskp, .tskc');
+    toggleViewSelector('.tskp, .tskc');
     viewAll = !viewAll;
       window.toggleViewAll.innerText = viewAll ? '한국어판만 보기' : '모두 보기';
     localStorage.setItem('viewAll', viewAll);
   }
 
+  if (!viewAll) {
+    toggleViewSelector('.tskp, .tskc');
+  }
+
   window.toggleHighlightNew.onclick = function() {
-    toggleViewColor('.V1_4, .V1_41, .new');
+    toggleViewColor('.V1_5, .new');
     toggleViewSelector('.newShowList', 'list-item');
     highlightNew = !highlightNew;
     window.toggleHighlightNew.innerText = highlightNew ? '강조 끄기' : '신규 항목 강조';
     localStorage.setItem('highlightNew', highlightNew);
   }
 
-  var viewSelector = document.querySelectorAll('.scenarioToggleButton');
-  viewSelector.forEach(function (x) {
-    x.onclick = function() {
-      x.innerText = x.innerText == '내용 보기' ? '내용 가리기' : '내용 보기';
-      toggleView(window[x.id.replace('Toggle', 'Div')], 'display', 'block');
-    };
-  });
-
-  if (!viewAll) {
-    toggleViewSelector('.eoep, eoec, .tskp, .tskc');
-  }
-
   if (highlightNew) {
-    toggleViewColor('.V1_4, .V1_41, .new');
+    toggleViewColor('.V1_5, .new');
     toggleViewSelector('.newShowList', 'list-item');
   }
 
   putCurrentItemClass(currentItemClass);
 }
+
+var viewSelector = document.querySelectorAll('.scenarioToggleButton');
+viewSelector.forEach(function (x) {
+  x.onclick = function() {
+    x.innerText = x.innerText == '내용 보기' ? '내용 가리기' : '내용 보기';
+    toggleView(window[x.id.replace('Toggle', 'Div')], 'display', 'block');
+  };
+});
 
 function toggleView (element, type, mode, initial) {
   if (!mode) {
